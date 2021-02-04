fb3cfe03d862a496ea53b7ed2971282438136e74
nadid <nadid@DESKTOP-I95TG51>
2021-02-04 16:35:26 +0300

    Add project files.

---
+++ DictonaryClass/MyDictonary.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictonaryClass
+{
+    class MyDictonary<K, V>
+    {
+        K[] keys;
+        V[] values;
+
+
+        public MyDictonary()
+        {
+            keys = new K[0];
+            values = new V[0];
+
+        }
+        public void Add(K key, V value)
+        {
+            K[] tempArrayKey = keys;
+            V[] tempArrayValue = values;
+            keys = new K[keys.Length + 1];
+            values = new V[values.Length + 1];
+            for (int i = 0; i < tempArrayKey.Length; i++)
+            {
+                keys[i] = tempArrayKey[i];
+                values[i] = tempArrayValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+        }
+        public void List()
+
+        {
+            for (int i = 0; i < keys.Length; i++)
+            {
+                Console.WriteLine("Kelimeler Ve Anlamları: " + keys[i] + "  " + values[i]);
+            }
+        }
+    }
+}
---
+++ DictonaryClass/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DictonaryClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictonary<int,string> dictonaryList = new MyDictonary<int,string>();
+            dictonaryList.Add(1,":"+ "Konsorsiyum:Uluslararası kimi kuruluşların ya da kimi devletlerin ekonomik ve mali yardımları yürütmek için oluşturdukları ortak organ.");
+            dictonaryList.Add(2,":"+ "Postmodernizm:Yazında ve daha çok da mimarlıkta, modern sonrası ortaya çıkan türlü yönelişlere verilen ad.");
+            dictonaryList.Add(3,":"+ "Konfederasyon:Egemenliği, kendi bağımsız hükümeti olmakla birlikte merkezi bir hükümetin yönetimini kabul eden devletlerin oluşturduğu birlik, devletler birliği.");
+            dictonaryList.List();
+        }
+    }
+}
